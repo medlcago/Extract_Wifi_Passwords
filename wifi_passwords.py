ea9ee508beb46794d31ce1620b25194488549ec8
Alexander <87269586+xMedicago@users.noreply.github.com>
2022-11-09 07:10:26 +0300

    Update wifi_passwords.py

--- wifi_passwords.py
+++ wifi_passwords.py
@@ -4,11 +4,11 @@ import subprocess
 def extract_wifi_passwords():
     """Extracting Windows Wi-Fi passwords into .txt file"""
 
-    profiles_data = subprocess.check_output('netsh wlan show profiles').decode('utf-8').split('\n')
+    profiles_data = subprocess.check_output('netsh wlan show profiles').decode('Windows-1251').split('\n')
     profiles = [i.split(':')[1].strip() for i in profiles_data if 'All User Profile' in i]
 
     for profile in profiles:
-        profile_info = subprocess.check_output(f'netsh wlan show profile {profile} key=clear').decode('utf-8').split(
+        profile_info = subprocess.check_output(f'netsh wlan show profile {profile} key=clear').decode('Windows-1251').split(
             '\n')
 
         try:
